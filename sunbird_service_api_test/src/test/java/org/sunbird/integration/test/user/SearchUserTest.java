f6df082c96c606b0f5fd32fe9117b9f29b0ac60b
rkhema2010 <hemamalini.rathinam@tibilsolutions.com>
2018-07-26 17:32:19 +0530

    #SB-4912 feat: Functional testcase

--- sunbird_service_api_test/src/test/java/org/sunbird/integration/test/user/SearchUserTest.java
+++ sunbird_service_api_test/src/test/java/org/sunbird/integration/test/user/SearchUserTest.java
@@ -4,48 +4,36 @@ import com.consol.citrus.annotations.CitrusTest;
 import com.consol.citrus.testng.CitrusParameters;
 import javax.ws.rs.core.MediaType;
 import org.springframework.http.HttpStatus;
-import org.sunbird.common.action.OrgUtil;
-import org.sunbird.common.action.UserUtil;
 import org.sunbird.integration.test.common.BaseCitrusTestRunner;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SearchUserTest extends BaseCitrusTestRunner {
 
-  public static final String BT_TEST_NAME_CREATE_USER_SUCCESS = "testCreateUserSuccessForSearch";
-  public static final String BT_TEST_NAME_CREATE_ROOT_ORG_SUCCESS = "testCreateRootOrgSuccess";
-
-  public static String UNIQUE_ID_GENERATION;
-  public static String ROOT_ORG_CHANNAL;
-
   public static final String TEST_NAME_SEARCH_USER_FAILURE_WITHOUT_ACCESS_TOKEN =
       "testSearchUserFailureWithoutAccessToken";
-  public static final String TEST_NAME_SEARCH_USER_FAILURE_WITH_EMPTY_BODY =
-      "testSearchUserFailureWithEmptyBody";
+  public static final String TEST_SEARCH_USER_BY_FIRST_NAME_SUCCESS =
+      "testSearchUserFirstNameSuccess";
+  public static final String TEST_SEARCH_USER_WITH_QUERY_PARAM_SUCCESS =
+      "testSearchUserWithQueryParamSuccess";
+  public static final String TEST_SEARCH_USER_BY_PHONE_SUCCESS = "testSearchUserByPhoneSuccess";
+  public static final String TEST_SEARCH_USER_BY_EMAIL_SUCCESS = "testSearchUserByEmailSuccess";
+  public static final String TEST_SEARCH_USER_BY_USER_NAME_SUCCESS =
+      "testSearchUserByUserNameSuccess";
+  public static final String TEST_SEARCH_USER_BY_EMPTY_FILTER_SUCCESS =
+      "testSearchUserByEmptyFilterSuccess";
 
-  public static final String TEST_NAME_SEARCH_USER_SUCCESS_WITH_FIRST_NAME =
-      "testSearchUserSuccessWithFirstName";
-  public static final String TEST_NAME_SEARCH_USER_SUCCESS_WITH_PHONE_NUMBER =
-      "testSearchUserSuccessWithPhoneNumber";
+  public static final String TEST_SEARCH_USER_BY_LIMIT_SUCCESS = "testSearchUserByLimitSuccess";
 
-  public static final String TEST_NAME_SEARCH_USER_SUCCESS_WITH_EMPTY_FILTER =
-      "testSearchUserSuccessWithEmptyFilter";
-  public static final String TEST_NAME_SEARCH_USER_SUCCESS_WITH_LIMIT =
-      "testSearchUserSuccessWithLimit";
-  public static final String TEST_NAME_SEARCH_USER_SUCCESS_WITH_UNKNOWN_FIELDS =
-      "testSearchUserSuccessWithUnknownFields";
+  public static final String TEST_SEARCH_USER_UNKNOWN_FIELDS_SUCCESS =
+      "testSearchUserByUnknownFieldsSuccess";
+  public static final String TEST_SEARCH_EMPTY_BODY_FAILURE = "testSearchUserByEmptyBodyFailure";
 
   public static final String TEMPLATE_DIR = "templates/user/search";
-  public static final String BT_CREATE_USER_TEMPLATE_DIR = "templates/user/create";
-  public static final String BT_ORG_CREATE_ORG_TEMPLATE_DIR = "templates/organisation/create";
-  public static int count = 0;
 
   private String getSearchUserUrl() {
-    return getLmsApiUriPath("/api/user/v1/search", "/v1/user/search");
-  }
 
-  public static String getCreateUserUrl(BaseCitrusTestRunner runner) {
-    return runner.getLmsApiUriPath("/api/user/v1/create", "/v1/user/create");
+    return getLmsApiUriPath("/api/user/v1/search", "/v1/user/search");
   }
 
   @DataProvider(name = "searchUserFailureDataProvider")
@@ -53,11 +41,24 @@ public class SearchUserTest extends BaseCitrusTestRunner {
 
     return new Object[][] {
       new Object[] {
-        TEST_NAME_SEARCH_USER_FAILURE_WITHOUT_ACCESS_TOKEN, false, HttpStatus.UNAUTHORIZED,
+        TEST_NAME_SEARCH_USER_FAILURE_WITHOUT_ACCESS_TOKEN, false, HttpStatus.UNAUTHORIZED
       },
-      new Object[] {
-        TEST_NAME_SEARCH_USER_FAILURE_WITH_EMPTY_BODY, true, HttpStatus.BAD_REQUEST,
-      },
+      new Object[] {TEST_SEARCH_EMPTY_BODY_FAILURE, true, HttpStatus.INTERNAL_SERVER_ERROR},
+    };
+  }
+
+  @DataProvider(name = "searchUserSuccessDataProvider")
+  public Object[][] searchUserSuccessDataProvider() {
+
+    return new Object[][] {
+      new Object[] {TEST_SEARCH_USER_BY_FIRST_NAME_SUCCESS, true, HttpStatus.OK},
+      new Object[] {TEST_SEARCH_USER_WITH_QUERY_PARAM_SUCCESS, true, HttpStatus.OK},
+      new Object[] {TEST_SEARCH_USER_BY_PHONE_SUCCESS, true, HttpStatus.OK},
+      new Object[] {TEST_SEARCH_USER_BY_EMAIL_SUCCESS, true, HttpStatus.OK},
+      new Object[] {TEST_SEARCH_USER_BY_USER_NAME_SUCCESS, true, HttpStatus.OK},
+      new Object[] {TEST_SEARCH_USER_BY_EMPTY_FILTER_SUCCESS, true, HttpStatus.OK},
+      new Object[] {TEST_SEARCH_USER_BY_LIMIT_SUCCESS, true, HttpStatus.OK},
+      new Object[] {TEST_SEARCH_USER_UNKNOWN_FIELDS_SUCCESS, true, HttpStatus.OK},
     };
   }
 
@@ -79,27 +80,12 @@ public class SearchUserTest extends BaseCitrusTestRunner {
         RESPONSE_JSON);
   }
 
-  @DataProvider(name = "searchUserSuccessDataProvider")
-  public Object[][] searchUserSuccessDataProvider() {
-
-    return new Object[][] {
-      new Object[] {TEST_NAME_SEARCH_USER_SUCCESS_WITH_FIRST_NAME, true, HttpStatus.OK},
-      new Object[] {TEST_NAME_SEARCH_USER_SUCCESS_WITH_PHONE_NUMBER, true, HttpStatus.OK},
-      new Object[] {TEST_NAME_SEARCH_USER_SUCCESS_WITH_EMPTY_FILTER, true, HttpStatus.OK},
-      new Object[] {TEST_NAME_SEARCH_USER_SUCCESS_WITH_LIMIT, true, HttpStatus.OK},
-      new Object[] {
-        TEST_NAME_SEARCH_USER_SUCCESS_WITH_UNKNOWN_FIELDS, true, HttpStatus.OK,
-      },
-    };
-  }
-
   @Test(dataProvider = "searchUserSuccessDataProvider")
   @CitrusParameters({"testName", "isAuthRequired", "httpStatusCode"})
   @CitrusTest
   public void testSearchUserSuccess(
       String testName, boolean isAuthRequired, HttpStatus httpStatusCode) {
     getAuthToken(this, isAuthRequired);
-    beforeTestUserSearch(testName);
     performPostTest(
         this,
         TEMPLATE_DIR,
@@ -110,32 +96,5 @@ public class SearchUserTest extends BaseCitrusTestRunner {
         isAuthRequired,
         httpStatusCode,
         RESPONSE_JSON);
-  }
-
-  private void beforeTestUserSearch(String testName) {
-
-    if (testName.equalsIgnoreCase(TEST_NAME_SEARCH_USER_SUCCESS_WITH_FIRST_NAME)
-        || testName.equalsIgnoreCase(TEST_NAME_SEARCH_USER_SUCCESS_WITH_PHONE_NUMBER)) {
-      if (count == 0) {
-        count = 1;
-        UNIQUE_ID_GENERATION = String.valueOf(System.currentTimeMillis()).substring(0, 10);
-        ROOT_ORG_CHANNAL = OrgUtil.getRootOrgChannel();
-        variable("rootOrgChannel", ROOT_ORG_CHANNAL);
-        variable("rootExternalId", OrgUtil.getRootOrgExternalId());
-        OrgUtil.createOrg(
-            this,
-            testContext,
-            BT_ORG_CREATE_ORG_TEMPLATE_DIR,
-            BT_TEST_NAME_CREATE_ROOT_ORG_SUCCESS,
-            HttpStatus.OK);
-
-        variable("uniqueId", UNIQUE_ID_GENERATION);
-        UserUtil.createUser(
-            this, testContext, BT_CREATE_USER_TEMPLATE_DIR, BT_TEST_NAME_CREATE_USER_SUCCESS);
-      } else {
-        variable("uniqueId", UNIQUE_ID_GENERATION);
-        variable("rootOrgChannel", ROOT_ORG_CHANNAL);
-      }
-    }
   }
 }
